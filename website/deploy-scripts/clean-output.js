ba103dc9d17ae460355a57ad657c6d8ea295c3d0
Luca E <elsen.luca@gmail.com>
2019-09-20 17:55:58 +0200

    public and debug deploy scripts, copyjob

---
+++ website/deploy-scripts/clean-output.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = path + "/" + file;
+
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    console.log(`Deleting directory "${path}"...`);
+    fs.rmdirSync(path);
+  }
+};
+
+
+let deletepath = null;
+for(let i = 0; i< process.argv.length; i++) {
+    let arg = process.argv[i];
+    console.log(arg);
+    if(arg.startsWith("--")) {
+        deletepath = arg.substr(2);
+    }
+}
+
+console.warn("clean! -> recursively deleting directory", deletepath);
+
+deleteFolderRecursive(deletepath);
+
+console.log("Successfully cleaned output directory!");
--- website/src/ts/index.ts
+++ website/src/ts/index.ts
@@ -1,5 +1,11 @@
+/// <reference types="jquery" />
+
 class lurch {
     constructor() {
         console.log("hello lurch2");
+        jQuery("body").on("click", ()=> {
+            alert("yeeeehaaw!");
+        });
+
     }
 }
